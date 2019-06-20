6ae6e8d6933f22633cf8c09d8f1ac007fb288fa2
enkefalos <48687501+enkefalos@users.noreply.github.com>
2019-06-20 14:32:59 +0300

    Update Homogenizer_GUI.py

--- Homogenizer_GUI.py
+++ Homogenizer_GUI.py
@@ -7,20 +7,7 @@ class Homogenizer_GUI:
         self.default_folder_expression = 'Default Folder'
         self.user_prefs = None
         self.last_button_pressed = None
-        self.main_window_layout = [
-            [sg.Text('Welcome to MRI Reconstruction Tool!', size=(40, 1), font=("Helvetica", 15))],
-            [sg.Text("First, browse a folder containing the scan folders of the required data.\n"
-                    "Then, choose the calculation you would like to make.\n"
-                    "For each calculation, mark 'V' if you would like to save the results and/or display the image.\n")],
-            [sg.Text('Your Folder', size=(15, 1), auto_size_text=False, justification='right'),
-            sg.InputText(self.current_folder_expression), sg.FolderBrowse()],
-            [sg.Radio('Calculate reconstructed images', "RADIO1", default=True), sg.Radio('Calculate field maps', "RADIO1"),
-            sg.Radio('Calculate interpolated total field map', "RADIO1", default=True)],
-            [sg.Checkbox('Save reconstructed images       '), sg.Checkbox('Save field maps       ', default=True), sg.Checkbox('Save interpolated total field map')],
-            [sg.Checkbox('Display image                          '), sg.Checkbox('Display image         ', default=True), sg.Checkbox('Display image')],
-            [sg.Text('_'  * 80)],
-            [sg.Submit(), sg.Cancel()]
-            ]
+        self.main_window_layout = None
         self.save_popup_layout = [
             [sg.Text("Browse a folder that you would like to save the data in.", size=(50, 1))],
             [sg.Text('Your Folder', size=(15, 1), auto_size_text=False, justification='right'),
@@ -28,8 +15,24 @@ class Homogenizer_GUI:
             [sg.Submit(), sg.Cancel()]
             ]
 
+    def set_main_layout(self):
+        self.main_window_layout = [
+            [sg.Text('Welcome to MRI Reconstruction Tool!', size=(40, 1), font=("Helvetica", 15))],
+            [sg.Text("First, browse a folder containing the scan folders of the required data.\n"
+                    "Then, choose the calculation you would like to make.\n"
+                    "For each calculation, mark 'V' if you would like to save the results and/or display the image.\n")],
+            [sg.Text('Your Folder', size=(15, 1), auto_size_text=False, justification='right'),
+            sg.InputText(self.current_folder_expression), sg.FolderBrowse()],
+            [sg.Radio('Calculate reconstructed images', "RADIO1"), sg.Radio('Calculate field maps', "RADIO1"),
+            sg.Radio('Calculate interpolated total field map', "RADIO1", default=True)],
+            [sg.Checkbox('Save reconstructed images       ', default=True), sg.Checkbox('Save field maps       ', default=True), sg.Checkbox('Save interpolated total field map', default=True)],
+            [sg.Checkbox('Display image                          '), sg.Checkbox('Display image         '), sg.Checkbox('Display image')],
+            [sg.Text('_'  * 80)],
+            [sg.Submit(), sg.Cancel()]
+            ]
+
     def open_main_window(self):
-        # Add other initialization?
+        self.set_main_layout()
         window = sg.Window('MRI Reconstruction Tool', default_element_size=(40, 1)).Layout(self.main_window_layout)
         self.last_button_pressed, self.user_prefs = window.Read()
         window.Close()
