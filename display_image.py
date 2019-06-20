2fd811fb30abc4a8ae24655095ab4a961f14b4f0
dekelv1 <48444589+dekelv1@users.noreply.github.com>
2019-06-20 10:37:46 +0300

    Add files via upload

---
+++ display_image.py
@@ -0,0 +1,8 @@
+def display_image(list_of_imgs):
+    '''
+    Displays the reconstructed image
+    ''' 
+    #abs_img    = abs(img)
+    for image in list_of_imgs:
+        plt.imshow(img)
+        plt.show()
